f1d6db5f4d5ac0de1111bd45ee1defb95180d41f
nemesidaa <salmonellarabies@gmail.com>
2024-05-25 02:39:28 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nemesidaa/gohashlist
+
+go 1.22.2
---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/nemesidaa/gohashlist/internal/set"
+)
+
+func TestIntersection(t *testing.T) {
+	setA := &set.Set{Set: map[interface{}]struct{}{"a": {}, "b": {}}}
+	setB := &set.Set{Set: map[interface{}]struct{}{"a": {}, "d": {}}}
+
+	expected := &set.Set{Set: map[interface{}]struct{}{"a": {}}}
+
+	result, err := set.Intersection(setA, setB)
+	if err != nil {
+		t.Fatalf("failed to calculate intersection: %v", err)
+	}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Expected %v, found %v", expected.Set, result.Set)
+	}
+}
